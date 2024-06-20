057351eee1971d231654cb8df2a40c441506c4e6
Angel <nglbelangel@gmail.com>
2024-06-21 01:36:20 +0800

    add logo for company of experience

--- src/components/ExperienceSection/ExperienceSection.js
+++ src/components/ExperienceSection/ExperienceSection.js
@@ -1,30 +1,35 @@
 import React from 'react';
-import { ExperienceContainer, ExperienceItem, Role, Company, Duration, Description } from './ExperienceSection.styles';
+import { ExperienceContainer, ExperienceItem, Role, Company, Duration, Description, CompanyLogo } from './ExperienceSection.styles';
+import companyLogo from '../../assets/companylogo.jfif';
 
 const experiences = [
   {
     role: 'Frontend Developer',
-    company: 'Tech Company A',
+    company: 'Tech Company',
     duration: 'Jan 2020 - Present',
-    description: 'Developed and maintained the front end for various web applications using React and Redux.',
+    description: 'Worked on various frontend projects using React and Redux. Improved site performance and implemented responsive designs.',
+    logo: companyLogo,
   },
   {
-    role: 'Junior Developer',
-    company: 'Tech Company B',
+    role: 'Software Developer',
+    company: 'Another Company',
     duration: 'Jun 2018 - Dec 2019',
-    description: 'Assisted in the development of user interfaces and contributed to the design of new features.',
+    description: 'Developed backend services and APIs. Collaborated with cross-functional teams to define, design, and ship new features.',
+    logo: companyLogo,
   },
 ];
 
 const ExperienceSection = () => (
   <ExperienceContainer id="experience">
-    <h2>Experience</h2>
     {experiences.map((experience, index) => (
       <ExperienceItem key={index}>
-        <Role>{experience.role}</Role>
-        <Company>{experience.company}</Company>
-        <Duration>{experience.duration}</Duration>
-        <Description>{experience.description}</Description>
+        <CompanyLogo src={experience.logo} alt={`${experience.company} logo`} />
+        <div>
+          <Role>{experience.role}</Role>
+          <Company>{experience.company}</Company>
+          <Duration>{experience.duration}</Duration>
+          <Description>{experience.description}</Description>
+        </div>
       </ExperienceItem>
     ))}
   </ExperienceContainer>
